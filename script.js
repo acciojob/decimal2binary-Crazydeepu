b4541588cf5b53f942ca0665e76ba6989ac16f01
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-09-08 18:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   // Write your code here
+	let binary[] = new int[20];
+	let i=0;
+
+	while(num>0) {
+		int r = num%2;
+		binary[i++] = r;
+		num = num /2;
+	}
+	for(let j=i-1;j>=0;j--){
+		console.log(binary[j]+"");
+	}
 }
 
 window.decimalToBinary = decimalToBinary;
