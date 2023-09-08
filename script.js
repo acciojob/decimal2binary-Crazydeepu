69804bdce01c2bb303137b7e9bbe6aafcbf31f4e
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-09-08 18:23:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function decimalToBinary(num) {
   // Write your code here
-	let binary[];
+	let binary=[];
 	let i=0;
 
 	while(num>0) {
-		int r = num%2;
+		let r = num%2;
 		binary[i++] = r;
 		num = num /2;
 	}
